c75129bf03010de7d466cb9578eab5cca4636f71
Nir Moshe <nir.moshe.nm@gmail.com>
2022-06-06 22:49:02 +0300

    GameSkelaton: Works! It has one butterfly and few viruses. Animation is working + drag and drop.

---
+++ app/src/main/java/com/nirmo/heroesgame/DefaultAnimatorListener.java
@@ -0,0 +1,25 @@
+package com.nirmo.heroesgame;
+
+import android.animation.Animator;
+
+public class DefaultAnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+}
